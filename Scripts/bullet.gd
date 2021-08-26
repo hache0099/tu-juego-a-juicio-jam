5fbb2d76500503674dab2c088f82d8492e7ffcba
hache0099 <81655011+hache0099@users.noreply.github.com>
2021-08-25 23:28:23 -0300

    añadido bullet

---
+++ Scripts/bullet.gd
@@ -0,0 +1,16 @@
+extends Area2D
+
+# Por ahora es un const, quizas lo cambie a variable segun avance
+const MOVE_SPEED : float = 300.0
+
+var move_direction : Vector2
+
+func _ready():
+	pass
+
+func _process(delta):
+	position += move_direction.normalized() * MOVE_SPEED * delta
+
+func set_move(dir):
+	move_direction = dir
+	print(dir)
--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -1,14 +1,24 @@
+# Por ahora es un Area2D, pero tambien podría ser un KineticBody2D
 extends Area2D
+
+var bullet = preload("res://Scenes/bullet.tscn")
 
 const SPEED : float = 100.0
 
 var target_pos : Vector2 = Vector2()
-var move_direction = Vector2()
+var move_direction : Vector2
 
 func _ready():
 	pass
 
 func _process(delta):
-	var move_direction = target_pos - position
+	move_direction = target_pos - global_position
 	if move_direction.length() > 10:
 		position += move_direction.normalized() * SPEED * delta
+
+func _on_shootingTime_timeout():
+	var v = bullet.instance()
+	v.set_move(move_direction)
+	v.set_position(global_position)
+
+	get_node("/root/game").add_child(v)
